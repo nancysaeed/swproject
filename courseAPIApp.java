22abb813eea9d66b5371d49d0dfd9fe65b980a62
AyaAhmedEliwa <45417911+AyaAhmedEliwa@users.noreply.github.com>
2019-04-19 20:55:32 +0200

    Add files via upload

---
+++ courseAPIApp.java
@@ -0,0 +1,15 @@
+package io.javasprings.springbootEval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class courseAPIApp {
+
+	public static void main(String[] args) {
+		
+		
+		SpringApplication.run(courseAPIApp.class, args);		
+	}
+	
+}
