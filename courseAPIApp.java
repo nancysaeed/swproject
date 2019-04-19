92b2500bbd09fa3a3e947c57c127ed7a208a7639
AyaAhmedEliwa <45417911+AyaAhmedEliwa@users.noreply.github.com>
2019-04-19 21:03:55 +0200

    Update courseAPIApp.java

--- courseAPIApp.java
+++ courseAPIApp.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = Controller.class)
 public class courseAPIApp {
 
 	public static void main(String[] args) {
