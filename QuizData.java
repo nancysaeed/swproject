22001c092effb6db37d695005151f0772e6db090
AyaAhmedEliwa <45417911+AyaAhmedEliwa@users.noreply.github.com>
2019-04-23 17:33:07 +0200

    Add files via upload

---
+++ QuizData.java
@@ -0,0 +1,27 @@
+package io.javasprings.springbootEval;
+
+public class QuizData {
+
+	String Title;
+    String SkillType;
+    
+ public QuizData(){}
+ 
+ public void setTitle(String cn)
+ {
+     Title = cn;
+ }
+ public String getTitle()
+ {
+     return Title;
+ }
+ 
+ public void setSkillType(String Skill)
+ {
+     SkillType  = Skill;
+ }
+ public String getSkillType()
+ {
+     return SkillType ;
+ }
+}
