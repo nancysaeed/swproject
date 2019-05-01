0872589dbd394dbc1ab6e5a9f3360fb7054d65d4
AyaAhmedEliwa <45417911+AyaAhmedEliwa@users.noreply.github.com>
2019-05-01 19:40:28 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -5,12 +5,22 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTopic {
+	
+	@Autowired
+	private RecommendQuizsToUser RQTU; 
+	
+	@RequestMapping("/quiz")
+	public List<RecommendedQ> getonlyone() throws IOException
+	{				
+		return RQTU.RecommendedQuizForUser();		
+	}
 	
 	@Autowired
 	private RecommendCompaniesToUser RCTU;
@@ -20,16 +30,25 @@ public class ControllerTopic {
 	public List<Recommended> getrecoCompanies() throws IOException
     {
     	return RCTU.RecommendedCompanyForUser();
-    }
-	//////////////////////////////////
-	@Autowired
-	private RecommendQuizsToUser RQTU; 
-	
-	@RequestMapping("/quiz")
-	public List<RecommendedQ> getonlyone() throws IOException
-	{				
-		return RQTU.RecommendedQuizForUser();		
 	}
-
+	
+	@Autowired
+	private RecommendCandidatesToComp RCTC;
+	
+	
+	@RequestMapping("/users")
+	public List<Recommended> getrecousers() throws IOException
+    {
+    	return RCTC.RecommendedUsersForCompany();
+	}
+	@Autowired
+	private RecommendCandidatesToComp C;
+	
+	
+	@RequestMapping("/sortedcandidates")
+	public List<Recommended> getAllsorted() throws IOException
+    {
+    	return C.SortCandidates();
+	}
+	
 }
-
