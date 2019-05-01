14eb3b39907921da516a4d7330c8c292556c56e0
ShroukSaied <45403578+ShroukSaied@users.noreply.github.com>
2019-05-01 19:26:09 +0200

    Add files via upload

---
+++ Recommended.java
@@ -0,0 +1,26 @@
+package io.javasprings.springbootEval;
+
+public class Recommended {
+	
+	String Rname;
+	
+	public Recommended () {}
+	
+	public Recommended(String n)
+	{
+		
+		Rname= n;
+	}
+	public void setREname(String na)
+	{
+		
+		Rname=na;
+	}
+	public String getREname()
+	{
+		
+		return Rname;
+	}
+	
+
+}
