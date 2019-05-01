969884626b69b0ae37b06875b7b5997da80e776b
nancysaeed <45460644+nancysaeed@users.noreply.github.com>
2019-05-01 18:14:45 +0200

    Add files via upload

---
+++ sumscores.java
@@ -0,0 +1,29 @@
+package io.javasprings.springbootEval;
+
+public class sumscores {
+	
+	public sumscores() {}
+	
+	int sum; 
+	int index;
+	
+	public void setsum(int s)
+	{
+		sum = s;
+	}
+	public int getsum()
+	{
+		return sum;
+	}
+	
+	public void setindex(int s)
+	{
+		index = s;
+	}
+	public int getindex()
+	{
+		return index;
+	}
+	
+
+}
